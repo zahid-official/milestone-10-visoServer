6a7ef7cd6d3635f77659b048153fd4dd5bd2e3e6
zahid-official <zahid.official8@gmail.com>
2024-12-08 10:45:47 +0600

    Visa Applications Data ReadOne

--- server/index.js
+++ server/index.js
@@ -67,6 +67,14 @@ async function run() {
     const result = await cursor.toArray();
     res.send(result);
   })
+
+  // readOne applicatons data
+  app.get('/applicationDetails/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await applicationsCollection.findOne(query);
+    res.send(result);
+  })
   
   // create 
   app.post('/visa', async(req, res) =>{
